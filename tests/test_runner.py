e9eede68f18409977ed51058482dbac09a2bc198
Chtis Contreras <christophercontreras2607@gmail.com>
2025-01-24 19:45:19 -0600

    Add test runner to discover and execute all unit tests in the tests directory

---
+++ tests/test_runner.py
@@ -0,0 +1,8 @@
+import unittest
+
+# Discover and run all tests in the tests directory
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    tests = loader.discover(start_dir=".", pattern="test_*.py")
+    testRunner = unittest.TextTestRunner()
+    testRunner.run(tests)
